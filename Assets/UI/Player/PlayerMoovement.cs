74ac2dac06f17f4da57e30a6fd650a45b19ece30
Adrian Drabik <adriandrabik@gmail.com>
2020-07-01 14:38:42 +0200

    poczatek

---
+++ Assets/UI/Player/PlayerMoovement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI.Player
+{
+    public class PlayerMoovement : MonoBehaviour
+    {
+        public Rigidbody PlayerBody;
+        private float _x;
+        private float _y;
+
+        void Update()
+        {
+            _x = Input.GetAxisRaw("Horizontal");
+            _y = Input.GetAxisRaw("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            PlayerBody.AddForce(new Vector3(_x * 10, 0, _y * 10));
+        }
+    }
+}
